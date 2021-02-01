33d12482859a343241cf6458363eefd9029f862e
frankpagan <frank@cocreate.app>
2021-02-01 18:54:11 +0000

    clean file tree

---
+++ src/commands/clone.js
@@ -0,0 +1,26 @@
+const utils_git = require('../utils/git.js');
+const shellJs = require('shelljs');
+const list_repositories = require('../list_repositories_clone');
+
+const prompt = require('prompt');
+
+prompt.start();
+
+const properties = [
+    {
+        name: 'user_git',
+    },
+    {
+        name: 'password_git',
+        hidden: true
+    }
+];
+
+prompt.get(properties,  function (err, result) {
+    if (err) { return console.error(err); }
+    let tmp_pwd = process.cwd()
+    list_repositories.forEach(repo=>{
+        shellJs.cd(repo.path);
+        utils_git.cloneRepository(repo,result);
+    });
+});
