f8c755d7bea3cc1e2b15a308f3011aa419619dce
Ubuntu <ubuntu@ip-172-31-90-175.ec2.internal>
2021-04-10 16:33:29 +0000

    repo cloning working

--- src/commands/clone.js
+++ src/commands/clone.js
@@ -20,7 +20,14 @@ prompt.get(properties,  function (err, result) {
     if (err) { return console.error(err); }
     let tmp_pwd = process.cwd()
     list_repositories.forEach(repo=>{
-        shellJs.cd(repo.path);
+        shellJs.cd(tmp_pwd);
+        var path_repo = repo.path.split('/');
+        path_repo.pop()
+        path_repo = path_repo.join('/')
+        //console.log("path_repo ",path_repo)
+        shellJs.mkdir('-p', path_repo)
+        shellJs.cd(path_repo);
+        //console.log("Cd actual ",process.cwd())
         utils_git.cloneRepository(repo,result);
     });
 });
