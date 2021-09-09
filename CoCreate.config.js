85de84921ceaf3a43471811a9d1283ab536d9d87
frank pagan <frank@cocreate.app>
2021-09-09 04:53:48 +0000

    fix: ci docs

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,13 +4,13 @@ module.exports = {
         "organization_Id": "5ff747727005da1c272740ab",
         "host": "general.cocreate.app"
     },
-    
-    "sources": [{
+    "sources": [
+        {
             "entry": "./docs/index.html",
             "collection": "files",
             "document_id": "60145dc49f64ba1680b86693",
             "key": "src",
-            "data":{
+            "data": {
                 "name": "index.html",
                 "path": "/docs/action/index.html",
                 "domains": [
@@ -22,21 +22,5 @@ module.exports = {
                 "website_id": "61381ed8829b690010a4f2b2"
             }
         }
-    ],
-
-	"extract": {
-		"directory": "./src/",
-		"extensions": [
-			"js",
-			"css",
-			"html"
-		],
-		"ignores": [
-			"node_modules",
-			"vendor",
-			"bower_components",
-			"archive"
-		]
-	}
+    ]
 }
-
