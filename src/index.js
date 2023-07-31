2bd2e26b28d3a7453ed4018b4155e4f29ae4b469
frankpagan <frank@cocreate.app>
2023-07-31 15:13:38 -0600

    feat: support type="submit" and form.action
    - Refactored the `CoCreateAction` object into separate functions.
    - Added a new `runSpecialAction` function to handle special actions such as events and timeouts.
    - Updated the `runAction` function to call `runSpecialAction` if the action is not found in the `actions` object.
    - Removed unnecessary code and unused variables.
    - Exported the `init` function as the default export.
    
    This commit refactors the `CoCreateAction` object into separate functions for better code organization and clarity. It also introduces a new function called `runSpecialAction` to handle special actions like events and timeouts. Some unnecessary code and unused variables were removed as part of this refactoring. The `init` function is now exported as the default export.

--- src/index.js
+++ src/index.js
@@ -1,153 +1,169 @@
 import { queryElements } from '@cocreate/utils'
 
-const CoCreateAction = {
-    attribute: 'actions',
-    actions: {},
+const attribute = 'actions'
+const actions = {}
 
-    /**
-     * name: string
-     * callback: function
-     * endEvent: string
-     **/
-    init: function (data) {
-        if (!Array.isArray(data))
-            data = [data]
-        for (let { name, callback, endEvent } of data) {
-            if (!Array.isArray(name))
-                name = [name]
-            for (let i = 0; i < name.length; i++) {
-                if (this.actions[name[i]])
-                    continue;
+/**
+ * name: string
+ * callback: function
+ * endEvent: string
+ **/
+function init(data) {
+    if (!Array.isArray(data))
+        data = [data]
+    for (let { name, callback, endEvent } of data) {
+        if (!Array.isArray(name))
+            name = [name]
+        for (let i = 0; i < name.length; i++) {
+            if (actions[name[i]])
+                continue;
 
-                this.actions[name[i]] = {
-                    name: name[i],
-                    callback,
-                    endEvent: endEvent || name[i]
-                }
-
-            }
-        }
-    },
-
-    initActions: function () {
-        const self = this;
-        document.addEventListener('click', function (event) {
-            let btn = event.target;
-            if (!btn.getAttribute('actions'))
-                btn = event.target.closest('[actions]');
-
-            if (!btn) return;
-            event.preventDefault();
-
-            let actions = (btn.getAttribute(self.attribute) || "").replace(/\s/g, '').split(',');
-            if (actions.length == 0) return;
-
-            let index = 0;
-            let stagedActions = [];
-            for (let action of actions) {
-                let [name, params] = action.split('(')
-                if (params)
-                    params = params.substring(0, (params.length - 1))
-
-                stagedActions.push({ name, params })
+            actions[name[i]] = {
+                name: name[i],
+                callback,
+                endEvent: endEvent || name[i]
             }
 
-            self.__runAction(stagedActions, index, btn);
-        })
-    },
+        }
+    }
+}
 
-    __runAction: function (actions, index, btn) {
+function initActions() {
+    document.addEventListener('click', function (event) {
+        let element = event.target;
+        if (!element.getAttribute(attribute))
+            element = event.target.closest(`[${attribute}]`);
 
-        if (index >= actions.length) {
+        if (element.tagName === 'form') {
+            const pattern = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(:\d{1,5})?(\/.*)?$/i;
+            if (pattern.test(url))
+                return form.submit()
+        }
 
-            if (index == actions.length) {
-                this.__runLink(btn);
+        if (!element) return;
+        event.preventDefault();
+
+        let actions = (element.getAttribute(attribute) || "").replace(/\s/g, '').split(',');
+        if (actions.length == 0) return;
+
+        let index = 0;
+        let stagedActions = [];
+        for (let action of actions) {
+            let [name, params] = action.split('(')
+            if (params)
+                params = params.substring(0, (params.length - 1))
+
+            stagedActions.push({ name, params })
+        }
+
+        runAction(stagedActions, index, element);
+    })
+}
+
+function runAction(actions, index, element) {
+    if (index >= actions.length) {
+        if (index == actions.length) {
+            runSubmit(element)
+            runLink(element);
+        }
+        return;
+    }
+
+    const currentAction = actions[index];
+    if (!currentAction) return
+
+    const actionName = currentAction.name
+    const action = actions[actionName];
+
+    if (action) {
+        document.addEventListener(action.endEvent, function () {
+            runNextAction(actions, index, element);
+        }, { once: true });
+
+        if (action.callback)
+            action.callback.call(null, { element, ...currentAction });
+        else
+            runNextAction(actions, index, element);
+
+    } else {
+        let status = runSpecialAction(actions, index, element, actionName, currentAction.params);
+        if (status === "next") {
+            runNextAction(actions, index, element);
+        }
+    }
+}
+
+function runSpecialAction(actions, index, element, actionName, params) {
+    if (!params) return "next";
+
+    switch (actionName) {
+        case 'event':
+            console.log("Waiting Event....");
+            document.addEventListener(params, () => {
+                console.log('Event Action (Received event from other section) ====== ' + params);
+                runNextAction(actions, index, element);
+            }, { once: true })
+            break;
+        case 'timeout':
+            let delayTime = parseInt(params);
+            if (delayTime > 0) {
+                setTimeout(function () {
+                    console.log("Timeout ======= " + params)
+                    runNextAction(actions, index, element);
+                }, parseInt(params));
             }
-            return;
-        }
-
-        const currentAction = actions[index];
-        if (!currentAction) return
-        const actionName = currentAction.name
-        const action = this.actions[actionName];
-
-        if (action) {
-            const self = this
-            document.addEventListener(action.endEvent, function () {
-                self.__runNextAction(actions, index, btn);
-            }, { once: true });
-
-            if (action.callback)
-                action.callback.call(null, { element: btn, ...currentAction });
-            else
-                this.__runNextAction(actions, index, btn);
-
-        }
-        else {
-            let status = this.__runSpecialAction(actions, index, btn, actionName, currentAction.params);
-            if (status === "next") {
-                this.__runNextAction(actions, index, btn);
+            break;
+        case 'action':
+            let elements = queryElements({ element, selector: params, type: 'selector' });
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].click();
             }
-        }
-    },
+            break;
+        case 'submit':
+            let form = closest('form');
+            if (form)
+                form.click();
+            break;
+        default:
+            return "next";
+    }
+}
 
-    __runSpecialAction: function (actions, index, btn, actionName, params) {
-        if (!params) return "next";
+function runNextAction(actions, index, element) {
+    runAction(actions, index += 1, element);
+}
 
-        const self = this;
-        switch (actionName) {
-            case 'event':
-                console.log("Waiting Event....");
-                document.addEventListener(params, () => {
-                    console.log('Event Action (Received event from other section) ====== ' + params);
-                    self.__runNextAction(actions, index, btn);
-                }, { once: true })
-                break;
-            case 'timeout':
-                let delayTime = parseInt(params);
-                if (delayTime > 0) {
-                    setTimeout(function () {
-                        console.log("Timeout ======= " + params)
-                        self.__runNextAction(actions, index, btn);
-                    }, parseInt(params));
-                }
-                break;
-            case 'action':
-                let btns = queryElements({ element: btn, selector: params, type: 'selector' });
-                for (let btn of btns) {
-                    btn.click();
-                }
-                break;
-            default:
-                return "next";
+function runSubmit(element) {
+    let button = element.closest('[type="submit"]');
+    if (!button)
+        button = element.querySelector('[type="submit"]');
+    if (button) {
+        let form = element.closest('form')
+        if (form && form.action) {
+            const pattern = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(:\d{1,5})?(\/.*)?$/i;
+            if (pattern.test(form.action))
+                form.submit()
         }
-    },
+    }
+}
 
-    __runNextAction: function (actions, index, btn) {
-        this.__runAction(actions, index += 1, btn);
-    },
+function runLink(element) {
+    let link = element.closest('[href], [target], [pass_to]');
+    if (!link)
+        link = element.querySelector('[href], [target], [pass_to]');
+    if (link)
+        run(link);
+}
 
-    __runLink: function (element) {
-        let link = element.closest('[href], [target], [pass_to]');
-        if (!link) {
-            let link = element.querySelector('[href], [target], [pass_to]');
-            if (!link) return;
-            this.__run(link);
-        }
-        this.__run(link);
-    },
-
-    __run: function (link) {
-        if (typeof CoCreate.link !== 'undefined') {
-            CoCreate.link.runLink(link)
-        }
-        else if (link.hasAttribute('href')) {
-            window.location.href = link.getAttribute('href');
-        }
-    },
+function run(link) {
+    if (typeof CoCreate.link !== 'undefined') {
+        CoCreate.link.runLink(link)
+    } else if (link.hasAttribute('href')) {
+        window.location.href = link.getAttribute('href');
+    }
 }
 
 
-CoCreateAction.initActions();
+initActions();
 
-export default CoCreateAction;
+export default { init };
