eb4884063a39ca7a669af52c6ecf94725fe62411
frankpagan <frank@cocreate.app>
2025-04-26 22:22:03 -0600

    fix: webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,70 @@
-
 const path = require("path");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { EsbuildPlugin } = require("esbuild-loader");
 const { FileUploader } = require("@cocreate/webpack");
 
-
 module.exports = async (env, argv) => {
-  const isProduction = argv && argv.mode === "production";
-  const config = {
-    entry: {
-      'CoCreate-actions': './src/index.js'
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: isProduction ? '[name].min.js' : '[name].js',
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      library: ['CoCreate', 'actions'],
-      globalObject: "this",
-      clean: true
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: isProduction ? '[name].min.css' : '[name].css',
-      }),
-      ,
-      new FileUploader(env, argv)
-    ],
-    mode: isProduction ? "production" : "development",
-    devtool: isProduction ? "source-map" : "eval-source-map",
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "esbuild-loader",
-            options: {
-              loader: 'js',
-              target: 'es2017'
-            }
-          },
-        },
-        {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"]
-        },
-      ],
-    },
-    optimization: {
-      minimize: isProduction,
-      minimizer: [
-        new EsbuildPlugin({
-          target: 'es2017',
-          css: true
-        }),
-        new CssMinimizerPlugin()
-      ],
-      splitChunks: {
-        cacheGroups: {
-          defaultVendors: false
-        },
-      }
-    },
-    performance: {
-      hints: isProduction ? "warning" : false
-    }
-  };
-  return config;
+	const isProduction = argv && argv.mode === "production";
+	const config = {
+		entry: {
+			"CoCreate-actions": "./src/index.js"
+		},
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: isProduction ? "[name].min.js" : "[name].js",
+			libraryTarget: "umd",
+			libraryExport: "default",
+			library: ["CoCreate", "${componentName}"],
+			globalObject: "this",
+			clean: true
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: isProduction ? "[name].min.css" : "[name].css"
+			}),
+			,
+			new FileUploader(env, argv)
+		],
+		mode: isProduction ? "production" : "development",
+		devtool: isProduction ? "source-map" : "eval-source-map",
+		module: {
+			rules: [
+				{
+					test: /.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "esbuild-loader",
+						options: {
+							loader: "js",
+							target: "es2017"
+						}
+					}
+				},
+				{
+					test: /.css$/i,
+					use: [MiniCssExtractPlugin.loader, "css-loader"]
+				}
+			]
+		},
+		optimization: {
+			minimize: isProduction,
+			minimizer: [
+				new EsbuildPlugin({
+					target: "es2017",
+					css: true
+				}),
+				new CssMinimizerPlugin()
+			],
+			splitChunks: {
+				cacheGroups: {
+					defaultVendors: false
+				}
+			}
+		},
+		performance: {
+			hints: isProduction ? "warning" : false
+		}
+	};
+	return config;
 };
